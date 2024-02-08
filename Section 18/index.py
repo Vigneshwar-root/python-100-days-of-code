65c2c437cd48c8d722546989dd536c76726842c1
Vigneshwar <vigneshwar.rkamaraj@gmail.com>
2024-02-08 21:39:56 +0530

    sample test changes in index.py file

--- Section 18/index.py
+++ Section 18/index.py
@@ -1,16 +1,22 @@
 import turtle as t
+import random
 
 tim = t.Turtle()
 
 colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 directions = [0, 90, 180, 270]
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+## Drawing different shapes
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
 
-for shape_side_n in range(3,11):
-    draw_shape(shape_side_n)
+# for shape_side_n in range(3,11):
+#     draw_shape(shape_side_n)
+
+for _ in range(200):
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 

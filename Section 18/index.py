9cb84de3348711a00fb8ef299409f358ef596e24
Vigneshwar <vigneshwar.rkamaraj@gmail.com>
2024-02-08 21:24:05 +0530

    Initial commit

---
+++ Section 18/index.py
@@ -0,0 +1,16 @@
+import turtle as t
+
+tim = t.Turtle()
+
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3,11):
+    draw_shape(shape_side_n)
+
